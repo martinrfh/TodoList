1b8669ce8906176d46759955aa5803f9b0a17c4d
martinrfh <ramtinrajabi922@gmail.com>
2024-03-18 15:07:23 +0330

    Define styles for home page component.

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,11 +2,15 @@ import { useContext } from "react";
 import todoContext from "./context/todoContext";
 import AddTodoInput from "./AddTodoInput";
 import TodoList from "./TodoList";
+import { Box, Heading } from "@chakra-ui/react";
 
 const HomePage = () => {
   const { todos, dispatch } = useContext(todoContext);
   return (
-    <>
+    <Box maxWidth="600px" margin="4rem auto" paddingX={2}>
+      <Heading textAlign="center" fontSize="5xl" marginBottom="2rem">
+        Todo List
+      </Heading>
       <AddTodoInput
         onAddTodo={(todo) =>
           dispatch({
@@ -27,7 +31,7 @@ const HomePage = () => {
           })
         }
       />
-    </>
+    </Box>
   );
 };
 
